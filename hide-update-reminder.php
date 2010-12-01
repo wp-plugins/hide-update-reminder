53dfc6c881b77cab343f36ce5947dbbd396d4f80
sarahG111 <sarahG111@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-01 10:21:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hide-update-reminder/tags/1.2@317413 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hide-update-reminder.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Hide Update Reminder
+Plugin URI: http://www.stuffbysarah.net/wordpress-plugins/remove-update-reminder/
+Description: Allows you to remove the upgrade Nag screen from view for Editors and below
+Author: Sarah Anderson
+Version: 1.2
+Author URI: http://www.stuffbysarah.net/
+
+Thanks to Viper007Bond for the code hints
+*/
+
+add_action('admin_init', 'run_chk_usr_lvl');
+
+function run_chk_usr_lvl($matches) {
+	global $userdata;
+	if (!current_user_can('update_plugins')) 
+		remove_action('admin_notices', 'update_nag', 3);
+}
+?>
